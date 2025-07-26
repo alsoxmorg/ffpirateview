d9b8a800f1159670ed815a4864031190b2743bf5
games and readers <timcooperlabs@yandex.com>
2025-07-26 08:48:35 -0500

    Delete ffpirateview directory

--- ffpirateview/scripts/install_localbin.sh
+++
@@ -1,5 +0,0 @@
-echo "cp src/ffpirate ~/.local/bin" ; cp src/ffpirate ~/.local/bin
-
-echo "cp src/png21f ~/.local/bin" ; cp src/png21f ~/.local/bin
-
-echo "cp src/1fview ~/.local/bin" ; cp src/1fview ~/.local/bin
